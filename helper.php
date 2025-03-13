74bea7d74dd28687840d3dffada743380a18aae3
Jirat Jaturanpinyo <143077173+MeowSky49887@users.noreply.github.com>
2025-03-13 11:55:28 +0700

    Update helper.php

--- helper.php
+++ helper.php
@@ -1,5 +1,9 @@
 <?php
-    $_NODEJS = json_decode(stripslashes(str_replace('`', '"', $argv[1])) ?? '{}', true);
+    $_NODEJS = json_decode(stripslashes(html_entity_decode($argv[1], ENT_QUOTES, 'UTF-8')) ?? '{}', true);
+
+    if (json_last_error() !== JSON_ERROR_NONE) {
+        echo "JSON Error: " . json_last_error_msg();
+    }
 
     foreach ($_NODEJS as $key => $value) {
         $_NODEJS[$key] = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
