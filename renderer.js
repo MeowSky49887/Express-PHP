e92f6628ee2205183d6664cc22318499d7ed9d98
Jirat Jaturanpinyo <143077173+MeowSky49887@users.noreply.github.com>
2025-03-13 11:43:32 +0700

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 const { exec } = require("child_process");
 const path = require("path");
+var escape = require('escape-html');
 
 const helperPath = path.resolve(__dirname, "helper.php");
 const phpPath = path.resolve(__dirname, "bin", "php.exe");
@@ -9,7 +10,7 @@ module.exports = (filePath, options, callback) => {
     delete options["settings"];
     delete options["_locals"];
     delete options["cache"];
-    const data = JSON.stringify(options || {}).replaceAll('"', '`');
+    const data = escape(JSON.stringify(options || {}));
 
     exec(`"${phpPath}" -d auto_prepend_file="${helperPath}" "${phpFile}" ${data}`, (err, stdout) => {
         if (err) return callback(err);
