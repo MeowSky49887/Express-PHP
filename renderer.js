8c910884373bfb19f49626606aa3e1278e4d2b86
Jirat Jaturanpinyo <143077173+MeowSky49887@users.noreply.github.com>
2025-03-19 12:36:28 +0700

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -3,6 +3,9 @@ const os = require("os");
 const { exec } = require("child_process");
 const path = require("path");
 
+const helperPath = path.resolve(__dirname, "helper.php");
+const phpPath = path.resolve(__dirname, "bin", "php.exe");
+
 module.exports = (filePath, options, callback) => {
     const phpFile = path.resolve(filePath);
     delete options["settings"];
